dee6279cd8f42c03ead046c9ce4a2227641feb65
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-08-01 18:23:36 +0900

    refactor deviceの判定を関数にまとめる

--- Assets/SchwesteriumLibrary/Input/LocalMultiplayManager.cs
+++ Assets/SchwesteriumLibrary/Input/LocalMultiplayManager.cs
@@ -21,12 +21,12 @@ namespace SchwesteriumLibrary.Input
         private readonly List<T> _activePlayers = new();
         private readonly HashSet<InputDevice> _pairedDevices = new();
 
-        private bool IsReceiving = false;
+        private bool _isReceiving = false;
         public int ActivePlayerCount => _activePlayers.Count;
 
         public LocalMultiplayManager(int maxPlayers, GameObject[] playerObjs, bool useKeyboard, bool initResister)
         {
-            if(maxPlayers != playerObjs.Length) { Debug.LogWarning($"最大プレイヤー数が異なります！ playerObjs {playerObjs.Length}, maxPlayers {maxPlayers}"); }
+            if (maxPlayers != playerObjs.Length) { Debug.LogWarning($"最大プレイヤー数が異なります！ playerObjs {playerObjs.Length}, maxPlayers {maxPlayers}"); }
 
             _playerPrefabs = playerObjs;
             _maxPlayers = maxPlayers;
@@ -45,20 +45,28 @@ namespace SchwesteriumLibrary.Input
             }
         }
 
+        public void Dispose()
+        {
+            InputSystem.onDeviceChange -= OnDeviceChange;
+        }
+
+        /// <summary>
+        /// 未割当のプレイヤーオブジェクトを非表示にする
+        /// </summary>
+        /// <param name="v"></param>
         public void SetActiveUnassignedPlayer(bool v)
         {
             for (int i = 0; i < _maxPlayers - _activePlayers.Count; ++i)
             {
-                Debug.Log(i);
                 _playerPrefabs[_playerPrefabs.Length - i - 1].SetActive(v);
             }
         }
 
-        public void StartReceiving() => IsReceiving = true;
-        public void EndReceiving() => IsReceiving = false;
+        public void StartRegisterDevice() => _isReceiving = true;
+        public void EndRegisterDevice() => _isReceiving = false;
 
         /// <summary>
-        /// 
+        /// 接続済みのデバイスをすべてプレイヤーに登録する
         /// </summary>
         public void RegisterAllDevices()
         {
@@ -66,37 +74,23 @@ namespace SchwesteriumLibrary.Input
 
             foreach (var device in devices)
             {
-                Debug.Log(device.name);
-
                 //プレイヤー数の上限チェック
                 if (_activePlayers.Count >= _maxPlayers) { return; }
 
                 //既に参加済みのデバイスは無視
                 if (_pairedDevices.Contains(device)) { continue; }
 
-                if (_useKeyboard)
+                if (IsAcceptableDevice(device))
                 {
-                    //ゲームパッドとキーボードを登録する
-                    if (device is Gamepad || device is Keyboard)
-                    {
-                        JoinPlayer(device);
-                    }
+                    JoinPlayer(device);
                 }
-                else
-                {
-                    //ゲームパッドのみ登録する
-                    if (device is Gamepad)
-                    {
-                        JoinPlayer(device);
-                    }
-                }
-
             }
         }
 
+        //接続されたデバイスを登録する
         private void RegisterDevice(InputDevice device)
         {
-            if (!IsReceiving) { return; }
+            if (!_isReceiving) { return; }
 
             //プレイヤー数の上限チェック
             if (_activePlayers.Count >= _maxPlayers) { return; }
@@ -104,24 +98,14 @@ namespace SchwesteriumLibrary.Input
             //既に参加済みのデバイスは無視
             if (_pairedDevices.Contains(device)) { return; }
 
-            if (_useKeyboard)
+            if (IsAcceptableDevice(device))
             {
-                //ゲームパッドとキーボードを登録する
-                if (device is Gamepad || device is Keyboard)
-                {
-                    JoinPlayer(device);
-                }
-            }
-            else
-            {
-                //ゲームパッドのみ登録する
-                if (device is Gamepad)
-                {
-                    JoinPlayer(device);
-                }
+                JoinPlayer(device);
             }
         }
 
+
+        //デバイスとInputHandlerを紐づける
         private void JoinPlayer(InputDevice device)
         {
             var playerId = _activePlayers.Count;
@@ -141,8 +125,12 @@ namespace SchwesteriumLibrary.Input
             _activePlayers.Add(handler);
             _pairedDevices.Add(device);
 
+#if UNITY_EDITOR
             Debug.Log($"Player{++playerId} が参加しました{device.displayName}");
+#endif
         }
+
+        private bool IsAcceptableDevice(InputDevice device) => _useKeyboard ? (device is Gamepad || device is Keyboard) : device is Gamepad;
 
         /// <summary>
         /// デバイスの接続状態が変化したとき（切断対応）
@@ -159,26 +147,29 @@ namespace SchwesteriumLibrary.Input
                     break;
 
                 case InputDeviceChange.Disconnected:
-                    if (!_pairedDevices.Contains(device)) return;
+                    if (!_pairedDevices.Contains(device)) { return; }
 
                     //切断されたデバイスのプレイヤーを探す
                     var disconnected = _activePlayers.Find(p => p.ParentDevice == device);
+
+                    if (disconnected == null)
+                    {
+                        Debug.LogWarning("対応するプレイヤーが見つかりません");
+                        return;
+                    }
 
                     disconnected.Leave(device);
 
                     _pairedDevices.Remove(device);
                     _activePlayers.Remove(disconnected);
 
-                    Debug.LogWarning($"{device.displayName} が切断されたわよ");
+#if UNITY_EDITOR
+                    Debug.LogWarning($"{device.displayName} が切断されました");
+#endif
                     break;
                 default:
                     break;
             }
-        }
-
-        public void Dispose()
-        {
-            InputSystem.onDeviceChange -= OnDeviceChange;
         }
     }
 }
