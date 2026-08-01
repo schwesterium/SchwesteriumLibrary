b69fc061237ac552610c0049960cd8ddd4e99998
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-08-01 19:27:11 +0900

    add InputSystemで使うコントロールパスをまとめたクラス

---
+++ Assets/SchwesteriumLibrary/Input/GamepadPaths.cs
@@ -0,0 +1,34 @@
+﻿/*
+Author : schwesterium
+Date   : 2026/08/01
+*/
+
+namespace SchwesteriumLibrary.Input.ContollePath
+{
+    public static class GamepadPaths
+    {
+        public const string DPAD = "<Gamepad>/dpad";
+        public const string DPAD_UP = "<Gamepad>/dpad/up";
+        public const string DPAD_DOWN = "<Gamepad>/dpad/down";
+        public const string DPAD_LEFT = "<Gamepad>/dpad/left";
+        public const string DPAD_RIGHT = "<Gamepad>/dpad/right";
+
+        public const string BUTTON_SOUTH = "<Gamepad>/buttonSouth";
+        public const string BUTTON_WEST = "<Gamepad>/buttonWest";
+        public const string BUTTON_NORTH = "<Gamepad>/buttonNorth";
+        public const string BUTTON_EAST = "<Gamepad>/buttonEast";
+
+        public const string LEFT_STICK = "<Gamepad>/leftStick";
+        public const string RIGHT_STICK = "<Gamepad>/rightStick";
+        public const string LEFT_STICK_PRESS = "<Gamepad>/leftStickPress";
+        public const string RIGHT_STICK_PRESS = "<Gamepad>/rightStickPress";
+
+        public const string LEFT_SHOULDER = "<Gamepad>/leftShoulder";
+        public const string RIGHT_SHOULDER = "<Gamepad>/rightShoulder";
+        public const string LEFT_TRIGGER = "<Gamepad>/leftTrigger";
+        public const string RIGHT_TRIGGER = "<Gamepad>/rightTrigger";
+
+        public const string START = "<Gamepad>/start";
+        public const string SELECT = "<Gamepad>/select";
+    }
+}
---
+++ Assets/SchwesteriumLibrary/Input/KeyboadPaths.cs
@@ -0,0 +1,146 @@
+﻿/*
+Author : schwesterium
+Date   : 2026/08/01
+*/
+
+namespace SchwesteriumLibrary.Input.ContollePath
+{
+    /// <summary>
+    /// InputSystem用 キーボードのコントロールパス
+    /// </summary>
+    public static class KeyboadPaths
+    {
+        public const string ESCAPE = "<Keyboard>/escape";
+        public const string SPACE = "<Keyboard>/space";
+        public const string ENTER = "<Keyboard>/enter";
+        public const string TAB = "<Keyboard>/tab";
+        public const string BACKQUOTE = "<Keyboard>/backquote";
+        public const string QUOTE = "<Keyboard>/quote";
+        public const string SEMICOLON = "<Keyboard>/semicolon";
+        public const string COMMA = "<Keyboard>/comma";
+        public const string PERIOD = "<Keyboard>/period";
+        public const string SLASH = "<Keyboard>/slash";
+        public const string BACKSLASH = "<Keyboard>/backslash";
+        public const string LEFT_BRACKET = "<Keyboard>/leftBracket";
+        public const string RIGHT_BRACKET = "<Keyboard>/rightBracket";
+        public const string MINUS = "<Keyboard>/minus";
+        public const string EQUALS = "<Keyboard>/equals";
+        public const string UP_ARROW = "<Keyboard>/upArrow";
+        public const string DOWN_ARROW = "<Keyboard>/downArrow";
+        public const string LEFT_ARROW = "<Keyboard>/leftArrow";
+        public const string RIGHT_ARROW = "<Keyboard>/rightArrow";
+
+        public const string A = "<Keyboard>/a";
+        public const string B = "<Keyboard>/b";
+        public const string C = "<Keyboard>/c";
+        public const string D = "<Keyboard>/d";
+        public const string E = "<Keyboard>/e";
+        public const string F = "<Keyboard>/f";
+        public const string G = "<Keyboard>/g";
+        public const string H = "<Keyboard>/h";
+        public const string I = "<Keyboard>/i";
+        public const string J = "<Keyboard>/j";
+        public const string K = "<Keyboard>/k";
+        public const string L = "<Keyboard>/l";
+        public const string M = "<Keyboard>/m";
+        public const string N = "<Keyboard>/n";
+        public const string O = "<Keyboard>/o";
+        public const string P = "<Keyboard>/p";
+        public const string Q = "<Keyboard>/q";
+        public const string R = "<Keyboard>/r";
+        public const string S = "<Keyboard>/s";
+        public const string T = "<Keyboard>/t";
+        public const string U = "<Keyboard>/u";
+        public const string V = "<Keyboard>/v";
+        public const string W = "<Keyboard>/w";
+        public const string X = "<Keyboard>/x";
+        public const string Y = "<Keyboard>/y";
+        public const string Z = "<Keyboard>/z";
+
+        public const string KEY_1 = "<Keyboard>/1";
+        public const string KEY_2 = "<Keyboard>/2";
+        public const string KEY_3 = "<Keyboard>/3";
+        public const string KEY_4 = "<Keyboard>/4";
+        public const string KEY_5 = "<Keyboard>/5";
+        public const string KEY_6 = "<Keyboard>/6";
+        public const string KEY_7 = "<Keyboard>/7";
+        public const string KEY_8 = "<Keyboard>/8";
+        public const string KEY_9 = "<Keyboard>/9";
+        public const string KEY_0 = "<Keyboard>/0";
+
+        public const string LEFT_SHIFT = "<Keyboard>/leftShift";
+        public const string RIGHT_SHIFT = "<Keyboard>/rightShift";
+        public const string SHIFT = "<Keyboard>/shift";
+        public const string LEFT_ALT = "<Keyboard>/leftAlt";
+        public const string RIGHT_ALT = "<Keyboard>/rightAlt";
+        public const string ALT = "<Keyboard>/alt";
+        public const string LEFT_CTRL = "<Keyboard>/leftCtrl";
+        public const string RIGHT_CTRL = "<Keyboard>/rightCtrl";
+        public const string CTRL = "<Keyboard>/ctrl";
+        public const string LEFT_META = "<Keyboard>/leftMeta";
+        public const string RIGHT_META = "<Keyboard>/rightMeta";
+        public const string CONTEXT_MENU = "<Keyboard>/contextMenu";
+
+        public const string BACKSPACE = "<Keyboard>/backspace";
+        public const string PAGE_DOWN = "<Keyboard>/pageDown";
+        public const string PAGE_UP = "<Keyboard>/pageUp";
+        public const string HOME = "<Keyboard>/home";
+        public const string END = "<Keyboard>/end";
+        public const string INSERT = "<Keyboard>/insert";
+        public const string DELETE = "<Keyboard>/delete";
+        public const string CAPS_LOCK = "<Keyboard>/capsLock";
+        public const string NUM_LOCK = "<Keyboard>/numLock";
+        public const string PRINT_SCREEN = "<Keyboard>/printScreen";
+        public const string SCROLL_LOCK = "<Keyboard>/scrollLock";
+        public const string PAUSE = "<Keyboard>/pause";
+
+        public const string NUMPAD_ENTER = "<Keyboard>/numpadEnter";
+        public const string NUMPAD_DIVIDE = "<Keyboard>/numpadDivide";
+        public const string NUMPAD_MULTIPLY = "<Keyboard>/numpadMultiply";
+        public const string NUMPAD_PLUS = "<Keyboard>/numpadPlus";
+        public const string NUMPAD_MINUS = "<Keyboard>/numpadMinus";
+        public const string NUMPAD_PERIOD = "<Keyboard>/numpadPeriod";
+        public const string NUMPAD_EQUALS = "<Keyboard>/numpadEquals";
+        public const string NUMPAD_1 = "<Keyboard>/numpad1";
+        public const string NUMPAD_2 = "<Keyboard>/numpad2";
+        public const string NUMPAD_3 = "<Keyboard>/numpad3";
+        public const string NUMPAD_4 = "<Keyboard>/numpad4";
+        public const string NUMPAD_5 = "<Keyboard>/numpad5";
+        public const string NUMPAD_6 = "<Keyboard>/numpad6";
+        public const string NUMPAD_7 = "<Keyboard>/numpad7";
+        public const string NUMPAD_8 = "<Keyboard>/numpad8";
+        public const string NUMPAD_9 = "<Keyboard>/numpad9";
+        public const string NUMPAD_0 = "<Keyboard>/numpad0";
+
+        public const string F1 = "<Keyboard>/f1";
+        public const string F2 = "<Keyboard>/f2";
+        public const string F3 = "<Keyboard>/f3";
+        public const string F4 = "<Keyboard>/f4";
+        public const string F5 = "<Keyboard>/f5";
+        public const string F6 = "<Keyboard>/f6";
+        public const string F7 = "<Keyboard>/f7";
+        public const string F8 = "<Keyboard>/f8";
+        public const string F9 = "<Keyboard>/f9";
+        public const string F10 = "<Keyboard>/f10";
+        public const string F11 = "<Keyboard>/f11";
+        public const string F12 = "<Keyboard>/f12";
+        public const string F13 = "<Keyboard>/f13";
+        public const string F14 = "<Keyboard>/f14";
+        public const string F15 = "<Keyboard>/f15";
+        public const string F16 = "<Keyboard>/f16";
+        public const string F17 = "<Keyboard>/f17";
+        public const string F18 = "<Keyboard>/f18";
+        public const string F19 = "<Keyboard>/f19";
+        public const string F20 = "<Keyboard>/f20";
+        public const string F21 = "<Keyboard>/f21";
+        public const string F22 = "<Keyboard>/f22";
+        public const string F23 = "<Keyboard>/f23";
+        public const string F24 = "<Keyboard>/f24";
+
+        public const string OEM1 = "<Keyboard>/OEM1";
+        public const string OEM2 = "<Keyboard>/OEM2";
+        public const string OEM3 = "<Keyboard>/OEM3";
+        public const string OEM4 = "<Keyboard>/OEM4";
+        public const string OEM5 = "<Keyboard>/OEM5";
+    }
+}
