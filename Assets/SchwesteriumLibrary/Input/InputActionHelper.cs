05a0c6cfee0504a108d21813ab587932a25a1bf9
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-08-01 19:27:31 +0900

    add InputActionHelper

---
+++ Assets/SchwesteriumLibrary/Input/InputActionHelper.cs
@@ -0,0 +1,29 @@
+﻿/*
+Author : schwesterium
+Date   : 2026/08/01
+*/
+
+using SchwesteriumLibrary.Input.ContollePath;
+using UnityEngine.InputSystem;
+
+namespace SchwesteriumLibrary.Input
+{
+    public static class InputActionHelper
+    {
+        //groups
+        public const string KEYBOARD = "Keyboard";
+        public const string GAMEPAD = "Gamepad";
+
+        public static void AddMoveAction(InputActionMap map, string actionName = "Move")
+        {
+            var moveAction = map.AddAction(actionName, InputActionType.Value);
+
+            moveAction.AddBinding(GamepadPaths.LEFT_STICK, groups: GAMEPAD);
+            moveAction.AddCompositeBinding("Dpad")
+                .With("Up", KeyboadPaths.W, groups: KEYBOARD)
+                .With("Down", KeyboadPaths.S, groups: KEYBOARD)
+                .With("Left", KeyboadPaths.A, groups: KEYBOARD)
+                .With("Right", KeyboadPaths.D, groups: KEYBOARD);
+        }
+    }
+}
