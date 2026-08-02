297b214df2540b9c60945744fbdc3918305bf1be
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-08-02 22:55:06 +0900

    add NFC Feature

--- Assets/SchwesteriumLibrary/NFC/NFCAPI.cs
+++ Assets/SchwesteriumLibrary/NFC/NFCAPI.cs
@@ -5,7 +5,6 @@ Date   : 2026/08/02
 
 using System;
 using System.Runtime.InteropServices;
-using UnityEngine;
 
 namespace SchwesteriumLibrary.NFC
 {
@@ -18,17 +17,119 @@ namespace SchwesteriumLibrary.NFC
 
     public static class NFCAPI
     {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SCARD_IO_REQUEST
+        {
+            public uint dwProtocol;
+            public int cbPciLength;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct SCARD_READERSTATE
+        {
+            [MarshalAs(UnmanagedType.LPWStr)]
+            public string szReader;
+            public IntPtr pvUserData;
+            public uint dwCurrentState;
+            public uint dwEventState;
+            public uint cbAtr;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 36)]
+            public byte[] rgbAtr;
+        }
+
         [DllImport("winscard.dll")]
-        public static extern int SCardEstablishContext(uint dwScope, IntPtr pvReserved1, IntPtr pvReserved2, ref IntPtr phContext);
+        public static extern uint SCardEstablishContext(uint dwScope, IntPtr pvReserved1, IntPtr pvReserved2, out IntPtr phContext);
 
         [DllImport("winscard.dll", EntryPoint = "SCardGetStatusChangeW", CharSet = CharSet.Unicode)]
-        public static extern int SCardGetStatusChange(IntPtr hContext, uint dwTimeout, ref IntPtr rgReaderStates, uint cReaders);
+        public static extern uint SCardGetStatusChange(IntPtr hContext, uint dwTimeout, [In, Out] SCARD_READERSTATE[] rgReaderStates, uint cReaders);
 
         [DllImport("winscard.dll", EntryPoint = "SCardListReadersW", CharSet = CharSet.Unicode)]
-        public static extern int SCardListReaders(IntPtr hContext, byte[] mszGroups, ref byte[] mszReaders, ref UIntPtr pcchReaders);
+        public static extern uint SCardListReaders(IntPtr hContext, byte[] mszGroups, byte[] mszReaders, ref uint pcchReaders);
 
         [DllImport("winscard.dll")]
-        public static extern int SCardReleaseContext(IntPtr hContext);
+        public static extern uint SCardReleaseContext(IntPtr hContext);
 
+        [DllImport("winscard.dll", EntryPoint = "SCardConnectW", CharSet = CharSet.Unicode)]
+        public static extern uint SCardConnect(IntPtr hContext, string szReader, uint dwShareMode, uint dwPreferredProtocols, out IntPtr phCard, out uint pdwActiveProtocol);
+
+        [DllImport("winscard.dll")]
+        public static extern uint SCardControl(IntPtr hContext, uint dwControlCode, IntPtr lpInBuffer, uint cbInBufferSize, ref IntPtr lpOutBuffer, uint cbOutBufferSize, ref UIntPtr lpBytesReturned);
+
+        [DllImport("winscard.dll")]
+        public static extern uint SCardTransmit(IntPtr hCard, ref SCARD_IO_REQUEST pioSendPci, byte[] pbSendBuffer, uint cbSendLength, IntPtr pioRecvPci, byte[] pbRecvBufferm, ref uint pcbRecvLength);
+
+        [DllImport("winscard.dll")]
+        public static extern uint SCardDisconnect(IntPtr hCard, uint dwDisposition);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        public static extern void FreeLibrary(IntPtr handle);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr handle, string procName);
+
+        //winscard.hにあるよ
+        public static IntPtr GetSCardT0Pci()
+        {
+            var handle = LoadLibrary("Winscard.dll");
+            var pci = GetProcAddress(handle, "g_rgSCardT0Pci");
+            FreeLibrary(handle);
+
+            return pci;
+        }
+        public static IntPtr GetSCardT1Pci()
+        {
+            var handle = LoadLibrary("Winscard.dll");
+            var pci = GetProcAddress(handle, "g_rgSCardT1Pci");
+            FreeLibrary(handle);
+
+            return pci;
+        }
+        public static IntPtr GetSCardRawPci()
+        {
+            var handle = LoadLibrary("Winscard.dll");
+            var pci = GetProcAddress(handle, "g_rgSCardRawPci");
+            FreeLibrary(handle);
+
+            return pci;
+        }
+
+        public const uint SCARD_S_SUCCESS = 0;
+        public const uint SCARD_E_TIMEOUT = 0x8010000A;
+        public const uint SCARD_E_NO_SMARTCARD = 0x8010000C;
+        public const uint SCARD_E_CANT_DISPOSE = 0x8010000E;
+        public const uint SCARD_E_NO_SERVICE = 0x8010001D;
+
+        public const uint SCARD_SCOPE_USER = 0;
+        public const uint SCARD_SCOPE_TERMINAL = 1;
+        public const uint SCARD_SCOPE_SYSTEM = 2;
+
+        public const int SCARD_STATE_UNAWARE = 0x0000;
+        public const int SCARD_STATE_CHANGED = 0x00000002;
+        public const int SCARD_STATE_PRESENT = 0x00000020;
+        public const UInt32 SCARD_STATE_EMPTY = 0x00000010;
+
+        public const int SCARD_SHARE_SHARED = 0x00000002;
+        public const int SCARD_SHARE_EXCLUSIVE = 0x00000001;
+        public const int SCARD_SHARE_DIRECT = 0x00000003;
+
+        public const int SCARD_PROTOCOL_T0 = 1;
+        public const int SCARD_PROTOCOL_T1 = 2;
+        public const int SCARD_PROTOCOL_RAW = 4;
+
+        public const int SCARD_LEAVE_CARD = 0;
+        public const int SCARD_RESET_CARD = 1;
+        public const int SCARD_UNPOWER_CARD = 2;
+        public const int SCARD_EJECT_CARD = 3;
+
+        public const int SCARD_UNKNOWN = 0x00000000;
+        public const int SCARD_ABSENT = 0x00000001;
+        public const int SCARD_PRESENT = 0x00000002;
+        public const int SCARD_SWALLOWED = 0x00000003;
+        public const int SCARD_POWERED = 0x00000004;
+        public const int SCARD_NEGOTIABLE = 0x00000005;
+        public const int SCARD_SPECIFICMODE = 0x00000006;
     }
 }
---
+++ Assets/SchwesteriumLibrary/NFC/NFCReader.cs
@@ -0,0 +1,217 @@
+﻿/*
+Author : schwesterium
+Date   : 2026/08/02
+*/
+
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+namespace SchwesteriumLibrary.NFC
+{
+    public class NFCReader
+    {
+        private IntPtr _hContext = IntPtr.Zero;
+        IntPtr _hCard = IntPtr.Zero;
+        //リーダーは一つだけ
+        private NFCAPI.SCARD_READERSTATE[] _readStates = new NFCAPI.SCARD_READERSTATE[1];
+
+        public async UniTask Run(CancellationToken token)
+        {
+            try
+            {
+                if (!TryEstablishContext()) { throw new Exception("Establich Context Failed"); }
+
+                if (!TrySelectReader()) { throw new Exception("Select Failed"); }
+
+                await UniTask.WaitForEndOfFrame(token);
+
+                if (!IsReaderPresent()) { throw new Exception("No Card"); }
+
+                if (!TryReadCard()) { throw new Exception("Read Failed"); }
+
+            }
+            catch (OperationCanceledException e)
+            {
+                Debug.LogException(e);
+                Release();
+
+                return;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                Release();
+
+                return;
+            }
+
+            Release();
+
+            return;
+        }
+
+        private bool TryEstablishContext()
+        {
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            //リソースマネージャーコンテキストの確立
+            r = NFCAPI.SCardEstablishContext(NFCAPI.SCARD_SCOPE_USER, IntPtr.Zero, IntPtr.Zero, out _hContext);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardEstablishContext Failed {r}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TrySelectReader()
+        {
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            //使用できる全リーダーの文字数を取得
+            uint readerCount = 0;
+            r = NFCAPI.SCardListReaders(_hContext, null, null, ref readerCount);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardListReaders Failed {r}");
+                return false;
+            }
+
+            //リーダーの文字列を取得
+            byte[] readers = new byte[readerCount * 2]; //マルチバイトなため*2
+            r = NFCAPI.SCardListReaders(_hContext, null, readers, ref readerCount);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardListReaders Failed {r}");
+                return false;
+            }
+
+            List<string> readerNames = Encoding.Unicode.GetString(readers).Split('\0').ToList();
+
+            _readStates[0].szReader = readerNames[0];
+            _readStates[0].dwEventState = NFCAPI.SCARD_STATE_UNAWARE;
+
+            //リーダーが機能するか
+            r = NFCAPI.SCardGetStatusChange(_hContext, 100, _readStates, (uint)_readStates.Length);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"Check SCardGetStatusChange Failed {r}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsReaderPresent()
+        {
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            //3秒間 カードがあるかの判別を行う
+            r = NFCAPI.SCardGetStatusChange(_hContext, 3000, _readStates, (uint)_readStates.Length);
+            if (r == NFCAPI.SCARD_S_SUCCESS)
+            {
+                if ((_readStates[0].dwEventState & NFCAPI.SCARD_STATE_PRESENT) == NFCAPI.SCARD_STATE_PRESENT)
+                {
+                    Debug.Log($"カードあるね");
+                }
+                else
+                {
+                    Debug.Log($"カードないね");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Read SCardGetStatusChange Failed {r}");
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryReadCard()
+        {
+            //SCardConnect
+            //SCardControl << なくてもよい
+            //SCardTransmit
+            //SCardDisconnect
+
+            uint pdwActiveProtocol = 0;
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            r = NFCAPI.SCardConnect(_hContext, _readStates[0].szReader, NFCAPI.SCARD_SHARE_EXCLUSIVE, NFCAPI.SCARD_PROTOCOL_T0 | NFCAPI.SCARD_PROTOCOL_T1, out _hCard, out pdwActiveProtocol);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardConnect Failed {r}");
+                return false;
+            }
+
+            NFCAPI.SCARD_IO_REQUEST request = new NFCAPI.SCARD_IO_REQUEST();
+            request.dwProtocol = pdwActiveProtocol;
+            request.cbPciLength = Marshal.SizeOf<NFCAPI.SCARD_IO_REQUEST>();
+
+            byte[] reciveBuffer = new byte[256 * 2];
+            uint reciveLength = (uint)reciveBuffer.Length;
+
+            //IDmを取得するAPDUコマンド
+            //https://learn.microsoft.com/ja-jp/windows-hardware/drivers/nfc/storage-card-requirements
+            byte[] apduCommand = { 0xFF, 0xCA, 0x00, 0x00, 0x00 };
+
+            try
+            {
+                r = NFCAPI.SCardTransmit(_hCard, ref request, apduCommand, (uint)apduCommand.Length, (IntPtr)null, reciveBuffer, ref reciveLength);
+            }
+            finally
+            {
+                Debug.Log("Disconnect");
+                Disconnect();
+            }
+
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardTransmit Failed {r}");
+                return false;
+            }
+
+            if (reciveLength < 2) { return false; }
+            byte sw1 = reciveBuffer[reciveLength - 2];
+            byte sw2 = reciveBuffer[reciveLength - 1];
+            if (sw1 != 0x90 || sw2 != 0x00)
+            {
+                Debug.LogWarning($"APDU Error: {sw1:X2}{sw2:X2}");
+                return false;
+            }
+
+            //受信データからIDmを抽出 
+            Debug.Log(BitConverter.ToString(reciveBuffer, 0, (int)reciveLength));
+
+            return true;
+        }
+
+        private void Disconnect()
+        {
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            r = NFCAPI.SCardDisconnect(_hCard, NFCAPI.SCARD_LEAVE_CARD);
+            if (r != NFCAPI.SCARD_S_SUCCESS)
+            {
+                Debug.LogWarning($"SCardDisconnect Failed {r}");
+            }
+        }
+
+        private bool Release()
+        {
+            var r = NFCAPI.SCARD_S_SUCCESS;
+
+            r = NFCAPI.SCardReleaseContext(_hContext);
+
+            return r == NFCAPI.SCARD_S_SUCCESS;
+        }
+    }
+}
