5cdb87dec16f5634e62d35bde90f28e7c05b938c
tosa <111439988+SveaGUN@users.noreply.github.com>
2026-09-10 23:13:13 +0900

    update Collections更新

---
+++ Assets/SchwesteriumLibrary/Collections/Pair.cs
@@ -0,0 +1,23 @@
+﻿/*
+Author : schwesterium
+Date   : 2026/09/10
+*/
+
+using System;
+
+namespace SchwesteriumLibrary.Collections
+{
+    //keyとvalueを保存しておくクラス
+    [Serializable]
+    public class Pair<T1, T2>
+    {
+        public T1 First;
+        public T2 Second;
+
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+    }
+}
--- Assets/SchwesteriumLibrary/Collections/SerializableDictionary.cs
+++ Assets/SchwesteriumLibrary/Collections/SerializableDictionary.cs
@@ -35,12 +35,17 @@ namespace SchwesteriumLibrary.Collections
             //生成したDictionaryにkeyとvalueを追加する
             foreach (var pair in _keyValueList)
             {
-                _dictionary.Add(pair.Key, pair.Value);
+                _dictionary.Add(pair.First, pair.Second);
             }
 
             return true;
         }
 
+        /// <summary>
+        /// 要素を追加する。#defineディレクティブ UNITY_EDITORが有効な場合はInspectorの更新も行う
+        /// </summary>
+        /// <param name="key">キー</param>
+        /// <param name="value">要素</param>
         public void AddElement(Tkey key, Tvalue value)
         {
             if(_dictionary == null)
@@ -50,25 +55,47 @@ namespace SchwesteriumLibrary.Collections
             }
 
             _dictionary.Add(key, value);
+
+#if UNITY_EDITOR
             //Inspector上の表示も更新する
             _keyValueList.Add(new Pair<Tkey, Tvalue>(key, value));
+#endif
         }
 
+        /// <summary>
+        /// keyの要素をvalueで更新する
+        /// </summary>
+        /// <param name="key">キー</param>
+        /// <param name="value">要素</param>
         public void UpdateElementValue(Tkey key, Tvalue value)
         {
             _dictionary[key] = value;
         }
 
+        /// <summary>
+        /// keyの値を取得する
+        /// </summary>
+        /// <param name="key">キー</param>
+        /// <returns>keyの値 Tvalue</returns>
         public Tvalue GetElement(Tkey key)
         {
             return _dictionary[key];
         }
 
+        /// <summary>
+        /// Dictionaryに指定したキーが含まれているかどうか
+        /// </summary>
+        /// <param name="key">キー</param>
+        /// <returns>指定したキーが含まれているかどうか</returns>
         public bool ContainsKey(Tkey key)
         {
             return _dictionary.ContainsKey(key);
         }
 
+#if UNITY_EDITOR
+        /// <summary>
+        /// Editor専用: Inspectorの更新を行う
+        /// </summary>
         public void UpdateList()
         {
             //前提として、_dictionaryが最新の状態、_keyValueListは更新前の状態
@@ -76,7 +103,7 @@ namespace SchwesteriumLibrary.Collections
             for (int i = 0; i < _keyValueList.Count; i++)
             {
                 //keyの取り出し
-                var key = _keyValueList[i].Key;
+                var key = _keyValueList[i].First;
 
                 //keyが存在する場合は、Listの要素を更新する
                 if (_dictionary.TryGetValue(key, out Tvalue value))
@@ -85,24 +112,6 @@ namespace SchwesteriumLibrary.Collections
                 }
             }
         }
-    }
-
-    //keyとvalueを保存しておくクラス
-    [Serializable]
-    public class Pair<Tkey, Tvalue>
-    {
-        [SerializeField]
-        private Tkey _key;
-        [SerializeField]
-        private Tvalue _value;
-
-        public Pair(Tkey key, Tvalue value)
-        {
-            _key = key;
-            _value = value;
-        }
-
-        public Tkey Key { get => _key; }
-        public Tvalue Value { get => _value; }
+#endif
     }
 }
